ce4917c9bcc1b89ec56e8d4f39e9442e50b53d7f
nikleo-eng <99094059+nikleo-eng@users.noreply.github.com>
2022-05-16 22:33:33 +0200

    the assertion depended on the machine timezone

--- src/bdd/java/it/unifi/dinfo/view/javafx/step/spec/UserJavaFxViewStep.java
+++ src/bdd/java/it/unifi/dinfo/view/javafx/step/spec/UserJavaFxViewStep.java
@@ -2,6 +2,11 @@ package it.unifi.dinfo.view.javafx.step.spec;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import org.testfx.framework.junit.ApplicationTest;
 
 import io.cucumber.java.en.Then;
@@ -17,9 +22,14 @@ public class UserJavaFxViewStep extends ApplicationTest {
 	}
 	
 	@Then("The log text shows that the last login is happened at {string}")
-	public void theLogTextShowsThatTheLastLoginIsHappenedAtIn(String in) {
+	public void theLogTextShowsThatTheLastLoginIsHappenedAtIn(String in) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat(UserJavaFxView.SDF_PATTERN, Locale.ITALIAN);
+		sdf.setLenient(true);
+		Date inDate = sdf.parse(in);
 		String lastLogText = lookup("#" + UserJavaFxView.LOG_TEXT_ID).queryText().getText();
-		assertThat(lastLogText).isEqualTo(UserJavaFxView.LOG_STARTING_TEXT + in);
+		assertThat(lastLogText).startsWith(UserJavaFxView.LOG_STARTING_TEXT);
+		Date logDate = sdf.parse(lastLogText.substring(UserJavaFxView.LOG_STARTING_TEXT.length()));
+		assertThat(inDate).isEqualTo(logDate);
 	}
 	
 	@When("The user clicks the Logout button")
